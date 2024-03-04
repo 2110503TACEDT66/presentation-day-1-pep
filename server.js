861fa9aa4abf1b0ff05ab8683e737f90662fa71c
Witchapat Jindanath <jwitchapat@gmail.com>
2024-03-04 20:46:00 +0700

    update config authuser server

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet = require('helmet');
-const {xss} = require('express-xss-sanitizer');
-const rateLimit = require('express-rate-limit');
-const hpp = require('hpp');
-const cors = require('cors');
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUI = require('swagger-ui-express');
 
 // Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -18,9 +10,10 @@ dotenv.config({path:'./config/config.env'});
 connectDB();
 
 // Route files
-const hospitals = require ('./routes/hospitals');
+const campgrounds = require ('./routes/campgrounds');
 const auth = require('./routes/auth');
-const appointments = require('./routes/appointments');
+const bookings = require('./routes/bookings');
+const reviews = require('./routes/reviews');
 
 const app = express();
 
@@ -30,53 +23,13 @@ app.use(express.json());
 // Cookie parser
 app.use(cookieParser());
 
-// Sanitize data
-app.use(mongoSanitize());
-
-// Set security headers
-app.use(helmet());
-
-// Prevent XSS attacks
-app.use(xss());
-
-// Rate Limiting
-const limiter = rateLimit({
-   windowsMs: 10 * 60 * 1000, // 10 mins
-   max: 100
-})
-app.use(limiter);
-
-// Prevent http param pollutions
-app.use(hpp());
-
-// Enable CORS
-app.use(cors());
-
-const swaggerOptions = {
-   swaggerDefinition:{
-      openapi: '3.0.0', 
-      info: {
-         title: 'Library API',
-         version: '1.0.0',
-         description: 'A simple Express VacQ API'
-      },
-      servers: [
-         {
-            url: 'http://localhost:5001/api/v1'
-         }
-      ],
-   },
-   apis:['./routes/*.js'], 
-};
-const swaggerDocs=swaggerJsDoc(swaggerOptions); 
-app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDocs));
-
 // Mount routers
-app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/campgrounds', campgrounds);
 app.use('/api/v1/auth', auth);
-app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/bookings', bookings);
+app.use('/api/v1/reviews', reviews)
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5002;
 const server = app.listen(PORT, console.log('Server running in ',process.env.NODE_ENV, ' mode on port ',PORT));
 
 // Handle unhandled promise rejections
@@ -86,3 +39,4 @@ process.on('unhandledRejection' , (err,promise)=> {
    // Close server & exit process
    server.close(()=>process.exit(1));
 });
+
